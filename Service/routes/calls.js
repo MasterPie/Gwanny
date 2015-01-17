345cd5f9ad294c02966b8002c56fc75817dec051
MasterPie <VivekPai@live.com>
2015-01-17 01:50:15 -0500

    EJS templating working

--- Service/routes/calls.js
+++ Service/routes/calls.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* POST make call */
-router.post('/:contactId', function(req, res, next) {
+router.get('/:contactId', function(req, res, next) {
 	var contactId = req.param("contactId");
 	
-	res.send('add to queue'); //do something
+	//res.send('add to queue'); //do something
+	
+	var response = {
+		weather: "58 degrees"
+	};
+	
+	res.render("call_made.html", response);
+	
 });
 
 module.exports = router;
