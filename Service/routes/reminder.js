85bbf99821c760112842aef9312158cd25cefb62
MasterPie <VivekPai@live.com>
2015-01-17 17:21:34 -0500

    News events finally working

--- Service/routes/reminder.js
+++ Service/routes/reminder.js
@@ -2,22 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 var apiKey = "Token ebe82805d059f2f355a4ff03d317b8070eb910a0";
-var currentName = "Philadelphia";
-var baseURL = "https://api.everyblock.com";
+var currentName = "philly";
+var baseURL = "api.everyblock.com";
 
-var http = require('http');
+var http = require('https');
 
 /* POST make call */
 router.get('/', function(req, res, next) {
 	
 	//res.send('add to queue'); //do something
-console.log("about to create client object");	
-	var client = http.request(80, baseUrl + "/content/" + currentName + "/topnews/");
-console.log("create client object");	
-	var header = {'Host': 'api.everyblock.com', 'Authorization' : apiKey};	
-	var request = client.request('GET', '/', header);
-console.log("created request");
-	
+
+	//var client = http.request(80, baseURL + "/content/" + currentName + "/topnews/");
+	var options = {
+		'host': baseURL, 
+		'path' : "/content/" + currentName + "/topnews", 
+		'port':443,
+		'method':"GET",
+		'headers' : 
+			{
+				'Authorization' : apiKey,
+				'Accept': "application/json",
+				'Content-Type': "application/json",
+				'Host': baseURL
+			}
+	};	
+	//var request = client.request('GET', '/', header);
+	/*
 	request.on('response', function (response) {
 	  console.log('STATUS: ' + response.statusCode);
 	  console.log('HEADERS: ' + JSON.stringify(response.headers));
@@ -26,26 +36,34 @@ console.log("created request");
 		console.log('BODY: ' + chunk);
 	  });
 	});
+	*/
 	
+	//console.log(request);
 	
-	console.log(request);
-	/*
-	http.get(baseUrl + "/content/" + currentName + "/topnews/", function(res){
+	var request = http.request(options, function(response){
+	
+	console.log(response);
 		var body = '';
 		
-		res.on('data', function(chunk){
+		response.on('data', function(chunk){
 			body += chunk;
+			console.log("data");
 		});
 		
-		res.on('end', function(){
-			var response = JSON.parse(body)
-			console.log(response);
+		response.on('end', function(){
+		console.log(body);
+			//var response = JSON.parse(body)
+			//console.log(response);
+			res.end(body);
 		});
+	}).on("error", function(e){
+		console.log("Error: " + e.message);
 	});
-	*/
-
 	
-	res.render("call_made.html", {});
+	request.end();
+
+
+	//res.render("call_made.html", {});
 	
 });
 
