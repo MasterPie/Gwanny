1cb097426fee62424d8bfad119d89bcb582fbd75
MasterPie <VivekPai@live.com>
2015-01-17 22:25:10 -0500

    Reminders hooked up

--- Service/routes/reminder.js
+++ Service/routes/reminder.js
@@ -10,60 +10,39 @@ var http = require('https');
 /* POST make call */
 router.get('/', function(req, res, next) {
 	
-	//res.send('add to queue'); //do something
-
-	//var client = http.request(80, baseURL + "/content/" + currentName + "/topnews/");
-	var options = {
-		'host': baseURL, 
-		'path' : "/content/" + currentName + "/topnews", 
-		'port':443,
-		'method':"GET",
-		'headers' : 
+	var response = {
+		todos: 
+		[
 			{
-				'Authorization' : apiKey,
-				'Accept': "application/json",
-				'Content-Type': "application/json",
-				'Host': baseURL
+				time: "9:00 AM",
+				items: [
+					{
+						name: "Medication 1",
+						description: "Take 2 pills after breakfast"
+					},
+					{
+						name: "Medication 2",
+						description: "One fish oil."
+					}
+				]
+			},
+			{
+				time: "12:00 PM",
+				items: [
+					{
+						name: "Something else",
+						description: "Take 2 pills after breakfast"
+					},
+					{
+						name: "Another thing else",
+						description: "One fish oil."
+					}
+				]
 			}
-	};	
-	//var request = client.request('GET', '/', header);
-	/*
-	request.on('response', function (response) {
-	  console.log('STATUS: ' + response.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(response.headers));
-	  response.setEncoding('utf8');
-	  response.on('data', function (chunk) {
-		console.log('BODY: ' + chunk);
-	  });
-	});
-	*/
+		]
+	};
 	
-	//console.log(request);
-	
-	var request = http.request(options, function(response){
-	
-	console.log(response);
-		var body = '';
-		
-		response.on('data', function(chunk){
-			body += chunk;
-			console.log("data");
-		});
-		
-		response.on('end', function(){
-		console.log(body);
-			//var response = JSON.parse(body)
-			//console.log(response);
-			res.end(body);
-		});
-	}).on("error", function(e){
-		console.log("Error: " + e.message);
-	});
-	
-	request.end();
-
-
-	//res.render("call_made.html", {});
+	res.render("reminder.html", response);
 	
 });
 
